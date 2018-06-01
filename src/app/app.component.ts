afd2e33096904aaeb3d3e030cf7ea0121dbc6edb
Emilio Coronado <emilio.mobile@gmail.com>
2018-06-01 17:51:08 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'piro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'piro';
+}
